1174b42c5e18bbf43ce113a2149b474663716786
cxyxiaoyuyu.com <923996381@qq.com>
2020-08-15 22:59:57 +0800

    switch 数据绑定

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,18 @@
 <template>
   Switch Demo
-  <Switch />
+  <Switch :value="status" @input="status=$event" />
 
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
+import { ref } from 'vue'
 export default {
+  setup(){
+    const status = ref(false)
+    console.log(status)
+    return {status}
+  },
   components: {
     Switch
   }
