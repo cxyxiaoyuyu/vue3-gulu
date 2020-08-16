dd554353fb6b0043cd415d219f47ca4ce478bbf7
cxyxiaoyuyu.com <923996381@qq.com>
2020-08-16 16:51:45 +0800

    button 支持loading

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -24,7 +24,20 @@
    </div>
 
    <div class="block">支持disabled</div>
+   <div class="row">
+      <Button disabled>disabled</Button>
+      <!-- <Button :disabled="true">disabled</Button> -->
+      <Button disabled theme="primary">disabled</Button>
+      <Button disabled theme="success">disabled</Button>
+      <Button disabled theme="info">disabled</Button>
+      <Button disabled theme="warning">disabled</Button>
+   </div>
+
    <div class="block">支持加载动画</div>
+   <div class="row">
+      <Button :loading="loading1" @click="showLoading">{{text}}</Button>
+      <Button :loading="loading2">加载中</Button>
+   </div>
 
  </div>
   
@@ -32,7 +45,23 @@
 
 <script>
 import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 export default {
+   setup(){
+      const loading1 = ref(false)
+      const loading2 = ref(true)
+      const text = ref('点我出现loading')
+
+      const showLoading = () => {
+         loading1.value = true
+         text.value = '加载中'
+         setTimeout(()=>{
+            loading1.value = false
+            text.value = '点我出现loading'
+         },5000)
+      }
+      return { text,loading1, loading2, showLoading }
+   },
    components: {
       Button,
    }
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,8 @@
 <template>
-  <button :class="['gulu-button',`gulu-theme-${theme}`,`gulu-size-${size}`]">
+  <button :disabled="disabled"
+    :class="['gulu-button',`gulu-theme-${theme}`,`gulu-size-${size}`]">
       <slot />
+      <span v-if="loading" class="gulu-loading"></span>
   </button>
 </template>
 
@@ -14,6 +16,14 @@ export default {
         size: {
             type: String,
             default: 'normal'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        loading: {
+            type: Boolean,
+            default: false,
         }
     }
 }
@@ -32,6 +42,8 @@ $border-color: #d9d9d9;
     outline: none;
     border-radius: 4px;
     font-size: 13px;
+    display: inline-flex;
+    align-items: center;
     cursor: pointer;
     box-shadow: 0 1px 0 fade-out(black,0.95);
     &+& {
@@ -42,6 +54,13 @@ $border-color: #d9d9d9;
         border-color: #c6e2ff;
         background-color: #ecf5ff;
     }
+    &[disabled]{
+        color: #c0c4cc;
+        cursor: not-allowed;
+        background-image: none;
+        background-color: #fff;
+        border-color: #ebeef5;
+    }
     &.gulu-theme-primary {
         color: #fff;
         background-color: #409eff;
@@ -49,6 +68,11 @@ $border-color: #d9d9d9;
         &:hover,&:active {
             background: #66b1ff;
             border-color: #66b1ff;
+        }
+        &[disabled]{
+            color: #fff;
+            background-color: #a0cfff;
+            border-color: #a0cfff;
         }
     }
     &.gulu-theme-success {
@@ -59,6 +83,10 @@ $border-color: #d9d9d9;
             background: #85ce61;
             border-color: #85ce61;
         }
+        &[disabled]{
+            background-color: #b3e19d;
+            border-color: #b3e19d;
+        }
     }
     &.gulu-theme-info {
         color: #fff;
@@ -68,6 +96,10 @@ $border-color: #d9d9d9;
             background: #a6a9ad;
             border-color: #a6a9ad;
         }
+        &[disabled]{
+            background-color: #c8c9cc;
+            border-color: #c8c9cc;
+        }
     }
     &.gulu-theme-warning {
         color: #fff;
@@ -76,6 +108,10 @@ $border-color: #d9d9d9;
         &:hover,&active {
             background: #ebb563;
             border-color: #ebb563;
+        }
+        &[disabled]{
+            background-color: #f3d19e;
+            border-color: #f3d19e;
         }
     }
     &.gulu-size-big {
@@ -87,6 +123,25 @@ $border-color: #d9d9d9;
         height: 28px;
         padding: 0 10px;
         font-size: 12px;
+    }
+    > .gulu-loading {
+        display: inline-block;
+        margin-left: 4px;
+        width: 12px;
+        height: 12px;
+        border-width: 2px;
+        border-style: solid;
+        border-color: #abcdef #abcdef #abcdef transparent;
+        border-radius: 6px;
+        animation: gulu-spin 1s infinite linear;
+    }
+}
+@keyframes gulu-spin {
+    from {
+        transform: rotate(0deg)
+    }
+    to {
+        transform: rotate(360deg)
     }
 }
 </style>
